d1d69c9608f91f34d3c8c91435a5d29be3d1cfcc
silverTaurus11 <gayuhnh@gmail.com>
2025-10-01 16:38:23 +0700

    implement ci/cd for android

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'firebase_options.dart';
 import 'main.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env.prod");
   runApp(const ProviderScope(child: MyApp()));
 }
